6a3b00bbdd4f76e830e0b0a12652734256b25d8b
srinivasragi4 <129055185+srinivasragi4@users.noreply.github.com>
2024-04-06 14:18:59 +0530

    Add files via upload

---
+++ DecendingOrder.java
@@ -0,0 +1,12 @@
+class DecendingOrder 
+{
+	public static void main(String[] args) 
+	{
+	
+		System.out.println("Enter the number");
+		for(int i=10;i>=1;i--)
+		{
+			System.out.println(i);
+		}
+	}
+}
